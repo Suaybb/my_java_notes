44bbcbdaae923fcd840d11fb67df4664a5af0546
Suayb Kiris <suaybkiris@gmail.com>
2021-12-03 15:23:58 +0300

    Basic relationship example between the classes

---
+++ Introduction-to-OOP-with-Java/src/java_notes/ch07/university/Course.java
@@ -0,0 +1,15 @@
+package java_notes.ch07.university;
+
+import java.util.Date;
+
+public class Course {
+	
+	String code;
+	String name;
+	Department department;
+	Professor teacher;
+	Student[] students;
+	String[] days;
+	Date time;
+
+}
