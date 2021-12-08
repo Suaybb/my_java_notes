25ec124482563d2ed37885c34e557ee5c2f3ade9
Suayb Kiris <suaybkiris@gmail.com>
2021-12-09 02:45:18 +0300

    Inheritance concept in java

---
+++ Introduction-to-OOP-with-Java/src/java_notes/ch10/factories/factory1/Manager.java
@@ -0,0 +1,17 @@
+package java_notes.ch10.factories.factory1;
+
+public class Manager extends Employee{
+	
+	protected String departmentManaged;
+	
+	public static final int MANAGEMENT_PAYMENT = 5000;
+	
+	public Manager (int no, String name, int year, String workingDepartment, String departmentManaged) {
+		
+		super(no, name, year, workingDepartment);
+		this.departmentManaged = departmentManaged;
+		System.out.println("in Manager()");
+		
+	}
+	
+}
